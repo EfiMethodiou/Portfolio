000684375219f51326412439b08d1898afab7cf4
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2020-08-03 11:27:09 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,89 @@
+
+/*<!--SCRIPTS-->*/
+
+                       /*MENU FUNCTIONS*/
+/*open up menu*/
+function openMenu() {
+    document.getElementById("myNavigation").style.width = "250px";
+}
+/*close menu*/
+function closeMenu() {
+    document.getElementById("myNavigation").style.width = "0";
+}
+
+
+                      /*HEXAGON FUNCTIONS*/
+/*big to blue*/
+function colortoB() {
+            /*1*/document.getElementById("hexagonbig1").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00c9f9)";
+            /*2*/document.getElementById("hexagonbig2").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00c9f9)";
+}
+
+/*small to blue*/
+function colortoBs() {
+        /*1*/document.getElementById("hexagonsm1").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00c9f9)";
+        /*2*/document.getElementById("hexagonsm2").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00c9f9)";
+        /*3*/document.getElementById("hexagonsm3").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00c9f9)";
+        /*4*/document.getElementById("hexagonsm4").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00c9f9)";
+        /*5*/document.getElementById("hexagonsm5").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00c9f9)";
+}
+
+/*medium to green*/
+function colortoG() {
+        /*1*/ document.getElementById("hexagonmed1").style.backgroundImage
+        = "linear-gradient(90deg, #00fe2e, #00fe2e)";
+        /*2*/document.getElementById("hexagonmed2").style.backgroundImage
+        = "linear-gradient(90deg, #00fe2e, #00fe2e)";
+        /*3*/document.getElementById("hexagonmed3").style.backgroundImage
+        = "linear-gradient(90deg, #00fe2e, #00fe2e)";
+        /*4*/document.getElementById("hexagonmed4").style.backgroundImage
+        = "linear-gradient(90deg, #00fe2e, #00fe2e)";
+        /*5*/document.getElementById("hexagonmed5").style.backgroundImage
+        = "linear-gradient(90deg, #00fe2e, #00fe2e)";
+}
+
+/*back to original*/
+function colorOrig() {
+            /*big*/
+        /*1*/document.getElementById("hexagonbig1").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*2*/document.getElementById("hexagonbig2").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+
+            /*medium*/
+        /*1*/document.getElementById("hexagonmed1").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*2*/document.getElementById("hexagonmed2").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*3*/document.getElementById("hexagonmed3").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*4*/document.getElementById("hexagonmed4").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*5*/document.getElementById("hexagonmed5").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+
+            /*small*/
+        /*1*/document.getElementById("hexagonsm1").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*2*/document.getElementById("hexagonsm2").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*3*/document.getElementById("hexagonsm3").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*4*/document.getElementById("hexagonsm4").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+        /*5*/document.getElementById("hexagonsm5").style.backgroundImage
+        = "linear-gradient(90deg, #00c9f9, #00fe2e)";
+}
+
+
+
+
+
+
