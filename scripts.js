37f7814e923190cb119255ab070e56e9ba7e2662
Efi Methodiou <37740639+EfiMethodiou@users.noreply.github.com>
2021-07-10 18:43:53 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,10 +25,11 @@ function closeMenu() {
 var project1 = new Image();
 var project2 = new Image();
 var project3 = new Image();
+var project4 = new Image();
 project1.src = "img/projects/td2.png";
 project2.src = "img/projects/gam2.png";
 project3.src = "img/projects/exh2.png";
-project3.src = "img/projects/pr4_2.png";
+project4.src = "img/projects/pr4_2.png";
 
 
 /*HEXAGON FUNCTIONS*/
