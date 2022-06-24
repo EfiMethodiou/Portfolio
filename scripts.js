bf1eff57064f8d9cb5065dfa052dab32a3ab3dd3
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2022-06-24 14:12:52 +0200

    added new project to the portfolio

--- scripts.js
+++ scripts.js
@@ -26,81 +26,20 @@ var project1 = new Image();
 var project2 = new Image();
 var project3 = new Image();
 var project4 = new Image();
+var project5 = new Image();
 project1.src = "img/projects/td2.png";
 project2.src = "img/projects/gam2.png";
 project3.src = "img/projects/exh2.png";
 project4.src = "img/projects/pr4_2.png";
+project5.src = "img/projects/acl2.png";
 
 
 /*HEXAGON FUNCTIONS*/
-/*big to blue*/
-function colortoB() {
-  /*1*/ document.getElementById("hexagonbig1").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00c9f9)";
-  /*2*/ document.getElementById("hexagonbig2").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00c9f9)";
-}
 
-/*small to blue*/
-function colortoBs() {
-  /*1*/ document.getElementById("hexagonsm1").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00c9f9)";
-  /*2*/ document.getElementById("hexagonsm2").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00c9f9)";
-  /*3*/ document.getElementById("hexagonsm3").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00c9f9)";
-  /*4*/ document.getElementById("hexagonsm4").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00c9f9)";
-  /*5*/ document.getElementById("hexagonsm5").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00c9f9)";
-}
 
-/*medium to green*/
-function colortoG() {
-  /*1*/ document.getElementById("hexagonmed1").style.backgroundImage =
-    "linear-gradient(90deg, #00fe2e, #00fe2e)";
-  /*2*/ document.getElementById("hexagonmed2").style.backgroundImage =
-    "linear-gradient(90deg, #00fe2e, #00fe2e)";
-  /*3*/ document.getElementById("hexagonmed3").style.backgroundImage =
-    "linear-gradient(90deg, #00fe2e, #00fe2e)";
-  /*4*/ document.getElementById("hexagonmed4").style.backgroundImage =
-    "linear-gradient(90deg, #00fe2e, #00fe2e)";
-  /*5*/ document.getElementById("hexagonmed5").style.backgroundImage =
-    "linear-gradient(90deg, #00fe2e, #00fe2e)";
-}
 
-/*back to original*/
-function colorOrig() {
-  /*big*/
-  /*1*/ document.getElementById("hexagonbig1").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*2*/ document.getElementById("hexagonbig2").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
 
-  /*medium*/
-  /*1*/ document.getElementById("hexagonmed1").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*2*/ document.getElementById("hexagonmed2").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*3*/ document.getElementById("hexagonmed3").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*4*/ document.getElementById("hexagonmed4").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*5*/ document.getElementById("hexagonmed5").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
 
-  /*small*/
-  /*1*/ document.getElementById("hexagonsm1").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*2*/ document.getElementById("hexagonsm2").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*3*/ document.getElementById("hexagonsm3").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*4*/ document.getElementById("hexagonsm4").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-  /*5*/ document.getElementById("hexagonsm5").style.backgroundImage =
-    "linear-gradient(90deg, #00c9f9, #00fe2e)";
-}
 
 //todo project
 function openPrToDo() {
@@ -128,4 +67,13 @@ function openPrExh() {
 function closePrExh() {
   document.getElementById("pr_exh").style.display = "none";
   document.getElementById("exhBackg").style.display = "none";
+}
+//Airbnb_Clone project
+function openPrACl() {
+  document.getElementById("pr_ac").style.display = "block";
+  document.getElementById("acBackg").style.display = "block";
+}
+function closePrACl() {
+  document.getElementById("pr_ac").style.display = "none";
+  document.getElementById("acBackg").style.display = "none";
 }
